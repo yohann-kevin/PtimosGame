2ee34f11b5bcf93b91d957c347dee2546592b5ec
Kirua <yo44prg@icloud.com>
2021-02-28 22:36:32 +0100

    feat : add class Ia

---
+++ app/src/main/java/ptimos/lib/Ia.java
@@ -0,0 +1,109 @@
+package ptimos.lib;
+
+import ptimos.Game;
+
+public class Ia {
+    String type;
+    int range;
+    int probaRoar;
+    int probaAtk;
+    int probaMove;
+    int probaAtkMagic;
+    Game game;
+    Ptimos ptimo;
+
+    public Ia(String type,Game game,Ptimos ptimo) {
+        this.type = type;
+        this.range = game.range;
+        this.game = game;
+        this.ptimo = ptimo;
+    }
+
+    public void launchIa() {
+        System.out.println(this.type + " démarrage ia");
+        this.checkAtk();
+        this.checkRoar();
+        this.checkMove();
+        this.checkAtkMagic();
+        this.getHighProba();
+    }
+
+    public void checkAtk() {
+        if (this.ptimo.getDominance() > 55 || this.ptimo.getStress() > 85) {
+            System.out.println("atk");
+            this.probaAtk = 60;
+        } else if (this.ptimo.getDominance() > 30 && this.ptimo.getDominance() < 56) {
+            this.probaAtk = 30;
+        } else {
+            this.probaAtk = 15;
+        }
+    }
+
+    public void checkRoar() {
+        if (this.ptimo.getDominance() < 31 && this.range > 3 && this.range < 10) {
+            System.out.println("rugis");
+            this.probaRoar = 60;
+        }
+    }
+
+    public void checkMove() {
+        if (this.ptimo.getStress() > 85) {
+            this.probaAtkMagic = 30;
+        } else if (this.ptimo.getStress() < 86 && this.ptimo.getStress() > 75) {
+            this.probaMove = 60;
+        } else if (this.ptimo.getStress() < 76 && this.ptimo.getStress() > 54) {
+            this.probaMove = 30;
+        } else if (this.ptimo.getStress() < 55) {
+            this.probaMove = 0;
+        }
+    }
+
+    public void checkAtkMagic() {
+        if (this.ptimo.getDominance() == 100) {
+            this.probaAtkMagic = 100;
+        } else if (this.range < 3) {
+            this.probaAtkMagic = 60;
+        }
+    }
+
+    public void getHighProba() {
+        System.out.println("dominance : " + this.ptimo.getDominance());
+        System.out.println("stress : " + this.ptimo.getStress());
+        System.out.println("range : " + this.range);
+        int[] allProba = {this.probaRoar,this.probaAtk,this.probaMove,this.probaAtkMagic};
+        System.out.println("probaRoar : " + this.probaRoar);
+        System.out.println("probaAtk : " + this.probaAtk);
+        System.out.println("probaMove : " + this.probaMove);
+        System.out.println("probaAtkMagic : " + this.probaAtkMagic);
+        int bigNumIndex = 0;
+        for (int i = 0; i < allProba.length; i++) {
+            if (allProba[i] >= allProba[bigNumIndex]) bigNumIndex = i;
+        }
+        System.out.println(bigNumIndex);
+        this.makeDecision(bigNumIndex);
+    }
+
+    public void makeDecision(int index) {
+        switch (index) {
+            case 0 :
+                System.out.println("roar");
+                this.ptimo.roar();
+                break;
+            case 1 :
+                System.out.println("atk");
+                this.ptimo.attack();
+                break;
+            case 2 : 
+                System.out.println("move");
+                this.ptimo.moveAway(this.game);
+                break;
+            case 3 :
+                System.out.println("atk magic");
+                this.ptimo.magicAttack();
+                break;
+            default : 
+                System.out.println("roar");
+                this.ptimo.roar();
+        }
+    }
+}
--- app/src/main/java/ptimos/lib/Ptimos.java
+++ app/src/main/java/ptimos/lib/Ptimos.java
@@ -7,6 +7,7 @@ abstract public class Ptimos {
     protected int stress;
     protected int dominance;
     Human target;
+    // int riskAtkMagique;
 
     public Ptimos(String type,Human target) {
         this.type = type;
@@ -37,24 +38,32 @@ abstract public class Ptimos {
     // all method
     public void feedback(Game game) {
         System.out.println(this.type + " réagis !");
-        // this.moveAway(game);
+        Ia ia = new Ia(this.type,game,this);
+        ia.launchIa();
     }
 
     public void roar() {
+        System.out.println("Roar");
         int bonus  = new RandomNum(0, 20).generateRandomNum();
         this.dominance += bonus;
+        if (this.dominance > 100) this.dominance = 100;
         this.stress -= bonus;
+        if (this.stress < 0) this.stress = 0;
     }
 
     public void attack() {
+        System.out.println("Attack");
         int dps = new RandomNum(0, 20).generateRandomNum();
         this.target.setLife(dps);
     }
 
     public void moveAway(Game game) {
+        System.out.println("Move");
         int move = new RandomNum(2, 5).generateRandomNum();
         game.range += move;
     }
 
-    public void magicAttack() {}
+    public void magicAttack() {
+        System.out.println("atk magique");
+    }
 }
